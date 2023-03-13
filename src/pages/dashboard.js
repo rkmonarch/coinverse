8e392a5aa5e7f727d73cd6d838aa5acd56f882e6
Senali <senalidilumika@gmail.com>
2023-03-13 08:53:19 +0530

    Added theme

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,21 +1,33 @@
-import Button from '@/components/form-elements/button'
-import Input from '@/components/form-elements/input'
-import React from 'react'
+import Button from "@/components/form-elements/button";
+import Input from "@/components/form-elements/input";
+import React from "react";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { BiSun, BiMoon } from "react-icons/bi";
 
 const Dashboard = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <div>
+      <div>
+        <button aria-label="Toggle Mode" onClick={toggleColorMode}>
+          {colorMode === "light" ? (
+            <BiMoon size="25" className="text-black hover:cursor-pointer" />
+          ) : (
+            <BiSun size="20" className="text-white hover:cursor-pointer" />
+          )}
+        </button>
+      </div>
       <Input
-                        id="email"
-                        name="email"
-                        label="Email"
-                        placeholder="Email"
-                        type="email"
-                        // onChange={(e) => setEmail(e.target.value)}
-                      />
-                      <Button label="Register" onClick={() => {}} />
+        id="email"
+        name="email"
+        label="Email"
+        placeholder="Email"
+        type="email"
+        // onChange={(e) => setEmail(e.target.value)}
+      />
+      <Button label="Register" onClick={() => {}} />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
