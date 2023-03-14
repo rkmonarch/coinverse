3f2c292c4feb4028251c7e78543e1b3b88fd7d64
Rahul Kulkarni <rk@Rahuls-MacBook-Pro.local>
2023-03-14 11:46:34 +0530

    Added connectkit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,18 @@
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../../theme";
+import { WagmiConfig } from "wagmi";
+import { client } from "../utils/wagmi";
+import { ConnectKitProvider } from "connectkit";
 
 export default function App({ Component, pageProps }) {
   return (
+    <WagmiConfig client={client}>
+    <ConnectKitProvider theme="retro">
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
+      </ConnectKitProvider>
+    </WagmiConfig>
   );
 }
