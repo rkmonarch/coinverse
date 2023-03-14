193a5cccc809b1c48dda664a4c2ad3687621703a
Aayush Gupta <guptaaayush432@gmail.com>
2023-03-14 17:11:02 +0530

    Updated deploy.js

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -1,32 +1,46 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so launchPadContract here is a factory for instances of our Launchpad contract.
+  */
+  const launchPadContract = await ethers.getContractFactory("MultiFactory");
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  // here we deploy the contract
+  const deployedLaunchPadContract = await launchPadContract.deploy(
+    "0x4E476F7FB84c785557cDECdbf8CADEbE8EA57C37"
   );
+
+  // Wait for it to finish deploying
+  await deployedLaunchPadContract.deployed();
+
+  // print the address of the deployed contract
+  console.log(
+    "Launchpad Contract Address:",
+    deployedLaunchPadContract.address
+  );
+
+  console.log("Sleeping.....");
+  // Wait for etherscan to notice that the contract has been deployed
+  await sleep(100000); // 100 seconds
+
+  // Verify the contract after deploying
+  await hre.run("verify:verify", {
+    address: deployedLaunchPadContract.address,
+    constructorArguments: ["0x4E476F7FB84c785557cDECdbf8CADEbE8EA57C37"],
+    contract: "contracts/MultiFactory.sol:MultiFactory",
+  });
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
