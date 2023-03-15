16975e146d83af51a033c25c3d61324f563a4065
Aayush Gupta <guptaaayush432@gmail.com>
2023-03-15 12:22:33 +0530

    Logic corrected

--- smart-contract/contracts/Token.sol
+++ smart-contract/contracts/Token.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
     uint internal totalCap;
-    uint internal initialMint;
+    uint internal totalTokenMinted;
     address internal creator;
 
 
@@ -18,30 +18,33 @@ contract Token is ERC20 {
 
     constructor(address _creator, string memory _name, string memory _symbol, bool _setTotalCap, uint _totalCap, bool _wantInitialMint, uint _initialMint, address[] memory _whiteListAddresses) ERC20(_name, _symbol) {
         totalCap = _totalCap;
-        initialMint = _initialMint;
+        totalTokenMinted = _initialMint;
         if(_setTotalCap == false){
             totalCap = type(uint256).max;
         }
         if(_wantInitialMint == false ){
-            initialMint = 0;
+            totalTokenMinted = 0;
         }
         for(uint i=0; i < _whiteListAddresses.length; i++){
             whiteList[_whiteListAddresses[i]] = true;
         }
         creator = _creator;
+        require(totalTokenMinted <= totalCap,"Limit exceed");
         _mint(_creator, _initialMint);
     }
 
-    function mint(address to, uint256 amount) public onlyAllowList {
-        _mint(to, amount);
+    function mint(address _to, uint256 _amount) public onlyAllowList {
+        require(totalTokenMinted + _amount <= totalCap,"Limit exceed");
+        _mint(_to, _amount);
+        totalTokenMinted += _amount;
     }
 
     function getTotalCap() public view returns(uint){
         return totalCap;
     }
 
-    function getInitialMint() public view returns(uint){
-        return initialMint;
+    function getTotalTokenMint() public view returns(uint){
+        return totalTokenMinted;
     }
 
     function getCreator() public view returns(address){
