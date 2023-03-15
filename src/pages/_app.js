24114ef37a307e344557dd9809635d04ff723b48
vrajdesai78 <vrajdesai78@gmail.com>
2023-03-15 12:33:18 +0530

    Added contract interaction logic

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { WagmiConfig } from "wagmi";
 import { client } from "../utils/wagmi";
-// import { ConnectKitProvider } from "connectkit";
+import { ConnectKitProvider } from "connectkit";
 import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }) {
